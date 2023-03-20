15c762e4efd77d8d6d56a69aee0b940d8b25f21b
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-20 20:27:18 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function window.customerName(){
+    return customerName;
+    }
+    console.log (window.customerName());
+ function upperCaseCustomerName(window.customerName){
+    retrun 'BOB';
+    }
+    console.log (upperCaseCustomerName());
+    
+    function setBestCustomer(){
+      let bestCustomer;
+         if (window.customerName){
+           return 'Maybe Bob';
+           }
+           else{
+          return 'not bob'
+          }
+        }
+   function changeLeastFavoriteCustomer() {
+          throw 'Assignment to constant variable.
+          };
+   
+        
